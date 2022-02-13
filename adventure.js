62a67c3edeb06e293c36a555e117326d90141ec1
orlandouchiha425 <valadez425@gmail.com>
2022-02-13 16:04:55 -0500

    created our enemy pizza rat

--- adventure.js
+++ adventure.js
@@ -3,13 +3,19 @@ class Hero{
         {
         this.name=name;
         this.health=100;
+        
         this.sprinkeSpray=5;
         this.sugarShock=10;
         this.catchPhrashes=['i\'m fresher than day old pizza',
         'you can\'t count my calories'];
+
+        
         }
      
-        
+        weapons={
+            sprinkleSpray: 5,
+            sugarShock: 10,
+          }
         talkSass(){
             return this.catchPhrashes[Math.floor(Math.random()* this.catchPhrashes.length)];
              }
@@ -24,3 +30,35 @@ class Hero{
 const dougie=new Hero ('Doughie the Donut');
 console.log(dougie.announceHealth())
 console.log(dougie.talkSass())
+// console.log(dougie.weapons[0])
+console.log(dougie.sugarShock)
+console.log(dougie.weapons.sprinkleSpray)
+
+
+
+
+class Enemy{
+    constructor(name)
+    {
+        this.health=100;
+        this.name=this.name;
+        this.catchPhrashes=['i\'m youtube famous',
+        'i\'m more dangerous than an uncovered sewer'];
+    }
+    weapons={
+        pepperoniStars: 5,
+        cheeseGrease: 10
+        
+      }
+      talkSmack(){
+        return this.catchPhrashes[Math.floor(Math.random()* this.catchPhrashes.length)];
+
+      }
+      fight(){
+          console.log('i\'m gonna flatten you like a slice of pepperoni!')
+      }
+}
+
+
+const pizza_Rat=new Enemy('Pizza Rat');
+console.log(pizza_Rat.talkSmack())
